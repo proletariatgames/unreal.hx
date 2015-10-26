bda33833cab1323e456f0cc8369d02adf90586c1
pauletariat <paul@proletariat.com>
2015-10-26 11:27:20 -0400

    Added PlayerArray to AGameState extern

--- Haxe/Externs/unreal/AGameState.hx
+++ Haxe/Externs/unreal/AGameState.hx
@@ -2,4 +2,8 @@ package unreal;
 
 @:glueCppIncludes("Engine.h")
 @:uextern extern class AGameState extends AInfo {
+
+  /** Array of all PlayerStates, maintained on both server and clients (PlayerStates are always relevant) */
+  @:uproperty(BlueprintReadOnly, Category=GameState)
+  var PlayerArray:PStruct<TArray<PStruct<APlayerState>>>;
 }
