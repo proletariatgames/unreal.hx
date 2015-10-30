7a5dfe5394692d1f68dbcb3d514478f1a64c6cfe
pauletariat <paul@proletariat.com>
2015-10-30 15:38:40 -0400

    Caue's fix for TypeRef to support new TArrays

---
+++ Haxe/Externs/unreal/TArrayImpl.hx
@@ -0,0 +1,21 @@
+package unreal;
+
+/**
+  The main Unreal Object class
+ **/
+@:glueCppIncludes("Containers/Array.h")
+@:uname("TArray")
+@:uextern extern class TArrayImpl<T>
+{
+  public function get_Item(index:Int):PRef<T>;
+  public function set_Item(index:Int, val:PRef<T>):Void;
+  public function Pop(allowShrinking:Bool):T;
+  public function Push(obj:PRef<PRef<T>>):Void;
+  public function SetNumUninitialized(arraySize:Int):Void;
+  public function Insert(item:PRef<PRef<T>>, index:Int):Int;
+  public function Num():Int;
+  public function Empty():Void;
+
+  @:uname('new') static function create<T>():PHaxeCreated<TArray<T>>;
+}
+
