f7da6ff9198f337ede2b3ef8b755ed40d20b73a4
David Lennon <dlennon@proletariat.com>
2015-11-03 09:08:31 -0500

    FGeometry externs for new/create and ToString() [rev: @waneck]

--- Haxe/Externs/unreal/FGeometry.hx
+++ Haxe/Externs/unreal/FGeometry.hx
@@ -2,4 +2,10 @@ package unreal;
 
 @:glueCppIncludes("Layout/Geometry.h")
 @:uextern extern class FGeometry {
+
+	@:uname('new')
+	static public function create() : PHaxeCreated<FGeometry>;
+
+	@:thisConst
+	public function ToString() : FString;
 }
