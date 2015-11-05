18509e3727f2fbc78fe57adf9f1b597c24704621
David Lennon <dlennon@proletariat.com>
2015-11-05 09:23:51 -0500

    Fix MatchState definitions (needed Const and no setter)

--- Haxe/Externs/unreal/AGameMode.hx
+++ Haxe/Externs/unreal/AGameMode.hx
@@ -3,10 +3,15 @@ package unreal;
 @:glueCppIncludes("Engine.h")
 @:uextern extern class AGameMode extends AInfo {
   @:global("MatchState")
-  public static var EnterinMap:PStruct<FName>;
-  public static var WaitingToStart:PStruct<FName>;
-  public static var InProgress:PStruct<FName>;
-  public static var WaitingPostMatch:PStruct<FName>;
-  public static var LeavingMap:PStruct<FName>;
-  public static var Aborted:PStruct<FName>;
+  public static var EnteringMap(default,never):Const<PStruct<FName>>;
+  @:global("MatchState")  
+  public static var WaitingToStart(default,never):Const<PStruct<FName>>;
+  @:global("MatchState")
+  public static var InProgress(default,never):Const<PStruct<FName>>;
+  @:global("MatchState")
+  public static var WaitingPostMatch(default,never):Const<PStruct<FName>>;
+  @:global("MatchState")
+  public static var LeavingMap(default,never):Const<PStruct<FName>>;
+  @:global("MatchState")
+  public static var Aborted(default,never):Const<PStruct<FName>>;
 }
