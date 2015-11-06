100c76b13f13e3e6b016b9a44713de1946fa6c2b
David Lennon <dlennon@proletariat.com>
2015-11-06 11:59:12 -0500

    Expose UGameInstance::Init for overriding in USeekGameInstanceBase

--- Haxe/Externs/unreal/UGameInstance.hx
+++ Haxe/Externs/unreal/UGameInstance.hx
@@ -3,6 +3,10 @@ package unreal;
 @:glueCppIncludes("Engine/GameInstance.h")
 @:uextern extern class UGameInstance extends UObject {
 
+	/** virtual function to allow custom GameInstances an opportunity to set up what it needs */
+	@:uexpose
+	public function Init() : Void;
+
 	@:uproperty()
 	private var LocalPlayers : TArray<ULocalPlayer>; // List of locally participating players in this game instance	
 }
