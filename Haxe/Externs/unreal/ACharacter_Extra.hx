ec52e0d38d256cd226726c227d1c8cb248311d70
David Lennon <dlennon@proletariat.com>
2015-11-11 15:13:59 -0500

    ACharacter.GetMesh() extern (since it's not a FUNCTION)

--- Haxe/Externs/unreal/ACharacter_Extra.hx
+++ Haxe/Externs/unreal/ACharacter_Extra.hx
@@ -2,4 +2,6 @@ package unreal;
 
 extern class ACharacter_Extra {
   public function GetCharacterMovement() : UCharacterMovementComponent;
+
+  public function GetMesh() : USkeletalMeshComponent;
 }
