1708117d6e1d19a8c742ae2a344771a785b0e4c7
Cauê Waneck <waneck@gmail.com>
2015-11-15 20:34:14 -0500

    try to silence deprecated warnings on clang

--- Haxe/Static/ue4hx/internal/buf/CppWriter.hx
+++ Haxe/Static/ue4hx/internal/buf/CppWriter.hx
@@ -14,8 +14,17 @@ class CppWriter extends BaseWriter {
       '#include <$module.h>\n#include "Engine.h"\n';
     getIncludes(cpp);
 
+    // unfortunately there's no clean way to deal with deprecated functions for now; there's no
+    // way to detect them through UHT, so for now we'll just disable them
+    cpp << '#ifdef __clang__\n#pragma clang diagnostic push\n' +
+      '#pragma clang diagnostic ignored "-Wdeprecated-declarations"\n' +
+      '#endif\n';
+
     cpp << '\n' <<
       bufContents;
+
+    cpp << '#ifdef __clang__\n#pragma clang diagnostic pop\n' +
+      '#endif\n';
 
     return cpp.toString();
   }
