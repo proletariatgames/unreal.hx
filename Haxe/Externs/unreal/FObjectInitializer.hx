e3cfe8cedade32cd364d39376d6c69e8ffb3ab11
David Lennon <dlennon@proletariat.com>
2015-11-18 16:47:36 -0500

    FObjectInitializer fixes

--- Haxe/Externs/unreal/FObjectInitializer.hx
+++ Haxe/Externs/unreal/FObjectInitializer.hx
@@ -1,7 +1,14 @@
 package unreal;
 
+typedef Foo<T> = Const<PRef<FObjectInitializer>>;
+
 @:glueCppIncludes("Engine.h")
 @:noCopy @:noEquals @:uextern extern class FObjectInitializer {
   public static function Get() : PRef<FObjectInitializer>;
+
+  @:thisConst
   @:typeName public function CreateDefaultSubobject<T>(Outer:UObject, SubojectName:FName, bTransient:Bool) : PExternal<T>;
+
+  @:thisConst
+  @:typeName public function SetDefaultSubobjectClass<T>(SubojectName:FName) : Foo<T>;
 }
