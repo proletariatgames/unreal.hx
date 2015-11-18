c1267209a3881d58bcca18e6606efe91eba74b21
Dan Ogles <danogles@gmail.com>
2015-11-18 15:25:20 -0500

    .equals support for FIntPoint

--- Haxe/Externs/unreal/FIntPoint_Extra.hx
+++ Haxe/Externs/unreal/FIntPoint_Extra.hx
@@ -1,11 +1,11 @@
 package unreal;
 
+@:hasEquals
 extern class FIntPoint_Extra {
   @:uname("new")
   public static function createWithValues(x:Int32, y:Int32) : PHaxeCreated<FIntPoint>;
 
   @:uname('new') public static function createForceInit(e:EForceInit):PHaxeCreated<FIntPoint>;
-
 
   public var X : Int32;
   public var Y : Int32;
