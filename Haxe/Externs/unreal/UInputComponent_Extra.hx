d5a771cc3fadafdc81548533bc6c11dcbeece40a
David Lennon <dlennon@proletariat.com>
2015-11-18 16:48:06 -0500

    UInputActionComponent BindAction/BindAxis fixes.

--- Haxe/Externs/unreal/UInputComponent_Extra.hx
+++ Haxe/Externs/unreal/UInputComponent_Extra.hx
@@ -2,5 +2,16 @@ package unreal;
 
 extern class UInputComponent_Extra {
 
-  public function BindAction<UserClass>(actionName:Const<PRef<FName>>, keyEvent:EInputEvent, object:UserClass, func:MethodPointer<UserClass,Void->Void>) : PRef<FInputActionBinding>;
+  /**
+   * Binds a delegate function to an Action defined in the project settings.
+   * Returned reference is only guaranteed to be valid until another action is bound.
+   */
+  public function BindAction(actionName:Const<FName>, keyEvent:EInputEvent, object:UObject, func:MethodPointer<UObject,Void->Void>) : PRef<FInputActionBinding>;
+
+
+  /**
+   * Binds a delegate function an Axis defined in the project settings.
+   * Returned reference is only guaranteed to be valid until another axis is bound.
+   */
+  public function BindAxis(axisName:Const<FName>, object:UObject, func:MethodPointer<UObject,Float32->Void>) : PRef<FInputAxisBinding>;
 }
