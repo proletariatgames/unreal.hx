0f9e9262444a853d99e1539398c1e7b24504ac6c
David Lennon <dlennon@proletariat.com>
2015-11-23 13:49:22 -0500

    Remove Foo<T> accidental easter egg in FObjectInitializer.hx [rev: @waneck]   - some testing code accidentally left in

--- Haxe/Externs/unreal/FObjectInitializer.hx
+++ Haxe/Externs/unreal/FObjectInitializer.hx
@@ -1,7 +1,5 @@
 package unreal;
 
-typedef Foo<T> = Const<PRef<FObjectInitializer>>;
-
 @:glueCppIncludes("Engine.h")
 @:noCopy @:noEquals @:uextern extern class FObjectInitializer {
   public static function Get() : PRef<FObjectInitializer>;
@@ -10,5 +8,5 @@ typedef Foo<T> = Const<PRef<FObjectInitializer>>;
   @:typeName public function CreateDefaultSubobject<T>(Outer:UObject, SubojectName:FName, bTransient:Bool) : PExternal<T>;
 
   @:thisConst
-  @:typeName public function SetDefaultSubobjectClass<T>(SubojectName:FName) : Foo<T>;
+  @:typeName public function SetDefaultSubobjectClass<T>(SubojectName:FName) : Const<PRef<FObjectInitializer>>;
 }
