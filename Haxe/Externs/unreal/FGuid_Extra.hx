92761f04cadbcb61cfe48f289dd053e8a5a5e6d4
Dan Ogles <danogles@gmail.com>
2015-11-30 13:11:06 -0500

    FGuid.NewGuid and UGameInstance.GetLocalPlayers

--- Haxe/Externs/unreal/FGuid_Extra.hx
+++ Haxe/Externs/unreal/FGuid_Extra.hx
@@ -4,6 +4,8 @@ extern class FGuid_Extra {
   @:uname('new')
   static public function create() : PHaxeCreated<FGuid>;
 
+  static public function NewGuid() : FGuid;
+
   @:thisConst
   public function ToString() : FString;
 }
--- Haxe/Externs/unreal/UGameInstance_Extra.hx
+++ Haxe/Externs/unreal/UGameInstance_Extra.hx
@@ -4,4 +4,7 @@ extern class UGameInstance_Extra {
   /** virtual function to allow custom GameInstances an opportunity to set up what it needs */
   @:uexpose
   public function Init() : Void;
+
+  @:thisConst
+  public function GetLocalPlayers() : Const<PRef<TArray<ULocalPlayer>>>;
 }
