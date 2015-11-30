cb89106e74362142a4f1312fe3ae50ac82e90e1b
Brett Davis <bdavis1000@gmail.com>
2015-11-30 11:37:23 -0500

    add some transform and vector methods

--- Haxe/Externs/unreal/FTransform_Extra.hx
+++ Haxe/Externs/unreal/FTransform_Extra.hx
@@ -9,9 +9,12 @@ extern class FTransform_Extra {
    * @param InTranslation The value to use for the translation component
    * @param InScale3D The value to use for the scale component
    */
-  @:uname('new') 
+  @:uname('new')
   public static function createRotatorTranslation(InRotation:Const<PRef<FRotator>>, InTranslation:Const<PRef<FVector>>) : PHaxeCreated<FTransform>;
 
   @:thisConst
   public function InverseTransformVectorNoScale(v:Const<PRef<FVector>>) : FVector;
+
+  @:thisConst
+  public function GetLocation() : FVector;
 }
