b782ddd0bf811c2ebfbef540666869ca82ccf7d4
Cauê Waneck <waneck@gmail.com>
2015-12-14 20:09:22 -0200

    Add StaticDuplicateObject for testing serialization

--- Haxe/Externs/unreal/UObject_Extra.hx
+++ Haxe/Externs/unreal/UObject_Extra.hx
@@ -72,6 +72,9 @@ extern class UObject_Extra {
   @:global public static function GetTransientPackage():UPackage;
 
   @:glueCppIncludes("UObject/UObjectGlobals.h")
+  @:global public static function StaticDuplicateObject(sourceObject:UObject, destOuter:UObject, destName:TCharStar):UObject;
+
+  @:glueCppIncludes("UObject/UObjectGlobals.h")
   @:typeName @:global public static function LoadObject<T>(outer:UObject, name:TCharStar, filename:TCharStar, loadFlags:Int, sandbow:UPackageMap) : PExternal<T>;
 
   public function PostEditImport() : Void;
