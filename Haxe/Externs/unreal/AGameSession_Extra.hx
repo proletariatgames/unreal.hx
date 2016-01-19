6d9f6db3e72a5e37bf4b3f544b1f5291a2223ae1
Cauê Waneck <waneck@gmail.com>
2016-01-18 22:46:06 -0200

    Add kick player on AGameSession

--- Haxe/Externs/unreal/AGameSession_Extra.hx
+++ Haxe/Externs/unreal/AGameSession_Extra.hx
@@ -5,4 +5,5 @@ extern class AGameSession_Extra
   public function HandleMatchIsWaitingToStart() : Void;
   public function HandleMatchHasStarted() : Void;
   public function HandleMatchHasEnded() : Void;
+  public function KickPlayer(kickerPlayer:APlayerController, kickReason:Const<PRef<FText>>):Bool;
 }
