452870e40890a39d37fb8d68c38f70018c5e3532
Brett Davis <bdavis1000@gmail.com>
2016-03-02 17:33:10 -0500

    add NativeConstruct extern

--- Haxe/Externs/unreal/umg/UUserWidget_Extra.hx
+++ Haxe/Externs/unreal/umg/UUserWidget_Extra.hx
@@ -6,4 +6,5 @@ extern class UUserWidget_Extra {
   @:global @:typeName
   public static function CreateWidget<T> (OwningPlayer:APlayerController, UserWidgetClass:UClass) : PExternal<T>;
   private function NativeTick(MyGeometry:Const<PRef<FGeometry>>, InDeltaTime:Float32):Void;
+  private function NativeConstruct():Void;
 }
