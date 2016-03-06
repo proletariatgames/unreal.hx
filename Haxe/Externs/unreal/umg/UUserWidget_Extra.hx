ea14bd892b7811d7e0644453969808f25a571eb3
Brett Davis <bdavis1000@gmail.com>
2016-03-06 18:24:16 -0500

    add user widget animation externs

--- Haxe/Externs/unreal/umg/UUserWidget_Extra.hx
+++ Haxe/Externs/unreal/umg/UUserWidget_Extra.hx
@@ -7,4 +7,6 @@ extern class UUserWidget_Extra {
   public static function CreateWidget<T> (OwningPlayer:APlayerController, UserWidgetClass:UClass) : PExternal<T>;
   private function NativeTick(MyGeometry:Const<PRef<FGeometry>>, InDeltaTime:Float32):Void;
   private function NativeConstruct():Void;
+  public function OnAnimationFinished_Implementation (Animation:Const<UWidgetAnimation>):Void;
+  public function OnAnimationStarted_Implementation (Animation:Const<UWidgetAnimation>):Void;
 }
