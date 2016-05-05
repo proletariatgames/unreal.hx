47061e24e7be66c1a561a6e8337d157f04f06526
ChetRippo <bdavis1000@gmail.com>
2016-05-05 16:15:44 -0400

    add mouse event externs to user widgets

--- Haxe/Externs/unreal/umg/UUserWidget_Extra.hx
+++ Haxe/Externs/unreal/umg/UUserWidget_Extra.hx
@@ -7,6 +7,8 @@ extern class UUserWidget_Extra {
   public static function CreateWidget<T> (OwningPlayer:APlayerController, UserWidgetClass:UClass) : PExternal<T>;
   private function NativeTick(MyGeometry:Const<PRef<FGeometry>>, InDeltaTime:Float32):Void;
   private function NativeConstruct():Void;
+  private function NativeOnMouseEnter(MyGeometry : Const<PRef<FGeometry>>, MouseEvent : Const<PRef<FPointerEvent>>) : Void;
+  private function NativeOnMouseLeave(InMouseEvent : Const<PRef<FPointerEvent>>) : Void;
   public function OnAnimationFinished_Implementation (Animation:Const<UWidgetAnimation>):Void;
   public function OnAnimationStarted_Implementation (Animation:Const<UWidgetAnimation>):Void;
 }
