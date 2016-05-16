0da673db6cbbbad344f18976226b391362f51cc8
Cauê Waneck <waneck@gmail.com>
2016-05-16 04:22:26 -0300

    For Haxe-created enums, we don't need to instantiate the enum glue helper
     * More C4996 warning disable

--- Haxe/Static/ue4hx/internal/UEnumBuild.hx
+++ Haxe/Static/ue4hx/internal/UEnumBuild.hx
@@ -41,8 +41,7 @@ class UEnumBuild
           enumType.meta.add(':utargetmodule', [macro $v{target}], enumType.pos);
         }
       }
-      var path = '$headerDir/Generated/Public/${uname.replace('.','/')}';
-      var headerPath = '$path.h';
+      var headerPath = '$headerDir/Generated/Public/${uname.replace('.','/')}.h';
       if (!FileSystem.exists('$headerDir/Generated/Public')) {
         FileSystem.createDirectory('$headerDir/Generated/Public');
       }
@@ -59,9 +58,7 @@ class UEnumBuild
       Globals.cur.hasUnprocessedTypes = true;
       Context.defineType(expose);
 
-      var writer = new HeaderWriter(headerPath),
-          cpp = new CppWriter('$headerDir/Generated/Private/${uname.replace('.','/'}.cpp');
-      cpp.include('${uname.replace('.','/')}.h');
+      var writer = new HeaderWriter(headerPath);
       writer.include('uhx/EnumGlue.h');
       writer.include('uhx/enums/${uname}_GetArray.h');
       writer.include('unreal/helpers/HxcppRuntime.h');
@@ -101,20 +98,17 @@ class UEnumBuild
       }
 
       writer.buf.add('};\n');
-      cpp.buf << 'namespace uhx {\n\n';
-      cpp.buf << 'template<> struct EnumGlue<$uname> {\n'
-        << '\tstatic $uname haxeToUe(unreal::UIntPtr haxe);\n'
-        << '\tstatic unreal::UIntPtr ueToHaxe($uname ue);\n'
-        << '};\n';
-      cpp.buf << '}\n\n'
-        << '$uname uhx::EnumGlue< $uname >::haxeToUe(unreal::UIntPtr haxe) {\n'
-          << '\treturn ($uname) unreal::helpers::HxcppRuntime::enumIndex(haxe);\n}\n'
-        << 'unreal::UIntPtr ueToHaxe($uname ue) {\n'
-          << '\tstatic unreal::UIntPtr array = uhx::enums::${uname}_GetArray::getArray();\n'
-          << '\treturn unreal::helpers::HxcppRuntime::arrayIndex(array, (int) ue);\n}\n\n';
+      writer.buf << 'namespace uhx {\n\n';
+      writer.buf << 'template<> struct EnumGlue<$uname> {\n'
+        << '\tstatic $uname haxeToUe(unreal::UIntPtr haxe) {\n'
+          << '\t\treturn ($uname) unreal::helpers::HxcppRuntime::enumIndex(haxe);\n}\n\n'
+        << '\tstatic unreal::UIntPtr ueToHaxe($uname ue) {\n'
+          << '\t\tstatic unreal::UIntPtr array = uhx::enums::${uname}_GetArray::getArray();\n'
+          << '\t\treturn unreal::helpers::HxcppRuntime::arrayIndex(array, (int) ue);\n}\n\n'
+          << '};\n';
+      writer.buf << '}';
 
       writer.close(Globals.cur.module);
-      cpp.close(Globals.cur.module);
 
 
     // case TAbstract(a, params):
