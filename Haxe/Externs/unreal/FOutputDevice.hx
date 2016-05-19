f5906419776b2231f6cd99530c5767011dffcaa3
Dan Brakeley <dan@brakeley.net>
2016-05-19 17:10:19 -0400

    expose more of FOutputDevice and FOutputDeviceRedirector

--- Haxe/Externs/unreal/FOutputDevice.hx
+++ Haxe/Externs/unreal/FOutputDevice.hx
@@ -22,8 +22,10 @@ package unreal;
   @:thisConst function CanBeUsedOnAnyThread():Bool;
 
   function Log(str:TCharStar):Void;
-
   @:uname('Log') function LogWithVerbosity(verbosity:ELogVerbosity, str:TCharStar):Void;
-
   @:uname('Log') function LogWithCategory(category:Const<FName>, verbosity:ELogVerbosity, str:TCharStar):Void;
+
+  function Serialize(data:Const<unreal.TCharStar>, verbosity:ELogVerbosity, category:Const<PRef<FName>>):Void;
+  @:uname("Serialize") function SerializeWithTime(
+    data:Const<unreal.TCharStar>, verbosity:ELogVerbosity, category:Const<PRef<FName>>, time:Const<unreal.Float64>):Void;
 }
