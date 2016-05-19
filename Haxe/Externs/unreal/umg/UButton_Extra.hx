7dfde4297dfcb6422fc27e8736ed7317f0fb8503
Cauê Waneck <waneck@gmail.com>
2016-05-19 17:33:05 -0300

    Fix UButton compilation error

--- Haxe/Externs/unreal/umg/UButton_Extra.hx
+++ Haxe/Externs/unreal/umg/UButton_Extra.hx
@@ -1,5 +1,6 @@
 package unreal.umg;
 
+@:glueCppIncludes("UMG.h", "Components/ContentWidget.h", "Components/Button.h")
 extern class UButton_Extra {
   public var OnClicked : FOnButtonClickedEvent;
 }
