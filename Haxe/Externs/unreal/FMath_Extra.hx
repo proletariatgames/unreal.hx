a2d00064e4274117848a2d84d7547376d17648b3
Cauê Waneck <waneck@gmail.com>
2016-05-20 12:03:31 -0300

    Add FMath.VInterpConstantTo

--- Haxe/Externs/unreal/FMath_Extra.hx
+++ Haxe/Externs/unreal/FMath_Extra.hx
@@ -2,7 +2,9 @@ package unreal;
 
 extern class FMath_Extra {
   public static function VInterpTo(Current : unreal.FVector, Target : unreal.FVector, DeltaTime : unreal.Float32, InterpSpeed : unreal.Float32) : unreal.FVector;
-  
+
+  public static function VInterpConstantTo(Current : unreal.FVector, Target : unreal.FVector, DeltaTime : unreal.Float32, InterpSpeed : unreal.Float32) : unreal.FVector;
+
   public static function Vector2DInterpTo(Current : unreal.FVector2D, Target : unreal.FVector2D, DeltaTime : unreal.Float32, InterpSpeed : unreal.Float32) : unreal.FVector2D;
 
   public static function RInterpTo(Current : unreal.FRotator, Target : unreal.FRotator, DeltaTime : unreal.Float32, InterpSpeed : unreal.Float32) : unreal.FRotator;
