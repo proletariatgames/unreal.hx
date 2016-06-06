24c0980c3ad3666168a3ffd959cd3adc0fb43e35
ChetRippo <bdavis1000@gmail.com>
2016-06-06 14:33:21 -0400

    UUserWidget::NativeOnFocusReceived and FReply externs

---
+++ Haxe/Externs/unreal/slatecore/FReply.hx
@@ -0,0 +1,8 @@
+package unreal.slatecore;
+
+
+@:umodule("SlateCore")
+@:glueCppIncludes("Input/Reply.h")
+@:noCopy @:noEquals @:uextern extern class FReply {
+  
+}
--- Haxe/Externs/unreal/umg/UUserWidget_Extra.hx
+++ Haxe/Externs/unreal/umg/UUserWidget_Extra.hx
@@ -14,6 +14,7 @@ extern class UUserWidget_Extra {
   private function NativeOnDragLeave(InDragDropEvent : Const<PRef<FDragDropEvent>>, InOperation : UDragDropOperation) : Void;
   private function NativeOnDragOver(InGeometry : Const<PRef<FGeometry>>, InDragDropEvent : Const<PRef<FDragDropEvent>>, InOperation : UDragDropOperation) : Bool;
   private function NativeOnDrop(InGeometry : Const<PRef<FGeometry>>, InDragDropEvent : Const<PRef<FDragDropEvent>>, InOperation : UDragDropOperation) : Bool;
+  private function NativeOnFocusReceived (InGeometry : Const<PRef<FGeometry>>, InFocusEvent : Const<PRef<FFocusEvent>>) : FReply;
   public function OnAnimationFinished_Implementation (Animation:Const<UWidgetAnimation>):Void;
   public function OnAnimationStarted_Implementation (Animation:Const<UWidgetAnimation>):Void;
 }
