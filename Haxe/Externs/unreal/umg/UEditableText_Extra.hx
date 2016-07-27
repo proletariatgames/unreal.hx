e585bef83a2de93992e668c3bce72cdf325043e4
Dan Ogles <danogles@gmail.com>
2016-07-27 16:45:16 -0400

    1. Fix bogus deprecation warnings in VC++ 2. Remove some traces 3. Patch out some externs that were duplicated or not working (const/ref enum)

--- Haxe/Externs/unreal/umg/UEditableText_Extra.hx
+++ Haxe/Externs/unreal/umg/UEditableText_Extra.hx
@@ -3,7 +3,5 @@ package unreal.umg;
 extern class UEditableText_Extra {
   function SetText(InText:FText) : Void;
   @:thisConst function GetText() : FText;
-  function SetIsPassword(isPassword:Bool) : Void;
   function SetHintText(hintText:FText) : Void;
-  function SetIsReadOnly(readOnly:Bool) : Void;
 }
--- Haxe/Static/ue4hx/internal/buf/CppWriter.hx
+++ Haxe/Static/ue4hx/internal/buf/CppWriter.hx
@@ -18,7 +18,7 @@ class CppWriter extends BaseWriter {
     cpp << '\n#ifdef __clang__\n#pragma clang diagnostic push\n' +
       '#pragma clang diagnostic ignored "-Wdeprecated-declarations"\n' +
       '#endif\n';
-    cpp << '#ifdef _MSVC_VER\n#pragma warning( disable : 4996 )\n#define _CRT_SECURE_NO_WARNINGS 1\n#define _CRT_SECURE_NO_WARNINGS_GLOBALS 1\n#define _CRT_SECURE_NO_DEPRECATE 1\n#endif\n'
+    cpp << '#ifdef _MSC_VER\n#pragma warning( disable : 4996 )\n#define _CRT_SECURE_NO_WARNINGS 1\n#define _CRT_SECURE_NO_WARNINGS_GLOBALS 1\n#define _CRT_SECURE_NO_DEPRECATE 1\n#endif\n'
       << '#include <$module.h>\n';
 
     if (!haxe.macro.Context.defined('UHX_NO_UOBJECT')) {
@@ -31,7 +31,7 @@ class CppWriter extends BaseWriter {
       bufContents;
 
     cpp << '\n#ifdef __clang__\n#pragma clang diagnostic pop\n#endif\n';
-    cpp << '#ifdef _MSVC_VER\n#undef _CRT_SECURE_NO_WARNINGS\n#undef _CRT_SECURE_NO_WARNINGS_GLOBALS\n#undef _CRT_SECURE_NO_DEPRECATE\n#pragma warning( default : 4996 )\n#endif\n';
+    cpp << '#ifdef _MSC_VER\n#undef _CRT_SECURE_NO_WARNINGS\n#undef _CRT_SECURE_NO_WARNINGS_GLOBALS\n#undef _CRT_SECURE_NO_DEPRECATE\n#pragma warning( default : 4996 )\n#endif\n';
 
     return cpp.toString();
   }
