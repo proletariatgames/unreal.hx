9df6af8280c0d420ea9d6970d6f9d0b8067ebdb7
dogles <dan@proletariat.com>
2016-08-16 18:52:53 -0400

    widget externs

---
+++ Haxe/Externs/unreal/umg/FWidgetTransform_Extra.hx
@@ -0,0 +1,5 @@
+package unreal.umg;
+
+extern class FWidgetTransform_Extra {
+  @:uname('.ctor') public static function create() : FWidgetTransform;
+}
--- Haxe/Externs/unreal/umg/UWidget_Extra.hx
+++ Haxe/Externs/unreal/umg/UWidget_Extra.hx
@@ -14,4 +14,15 @@ package unreal.umg;
 
   public function AddBinding(DelegateProperty:UDelegateProperty, SourceObject:UObject, BindingPath:Const<PRef<FDynamicPropertyPath>>) : Bool;
 
+  public function SetRenderTransform(transform:FWidgetTransform) : Void;
+
+  /** The render transform of the widget allows for arbitrary 2D transforms to be applied to the widget. */
+  var RenderTransform:FWidgetTransform;
+
+	/**
+	 * The render transform pivot controls the location about which transforms are applied.
+	 * This value is a normalized coordinate about which things like rotations will occur.
+	 */
+  var RenderTransformPivot:FVector2D;
+
 }
