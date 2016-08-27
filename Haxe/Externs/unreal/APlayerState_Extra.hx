bf9a6929241cd611ff9bcd5340e3ca3d165a5e8c
Ryan Smith <rsmith@ninerealms.net>
2016-08-26 19:20:28 -0500

    Exposing session name to haxe

--- Haxe/Externs/unreal/APlayerState_Extra.hx
+++ Haxe/Externs/unreal/APlayerState_Extra.hx
@@ -7,4 +7,9 @@ extern class APlayerState_Extra {
     Called by seamless travel when initializing a player on the other side - copy properties to the new PlayerState that should persist
    **/
   public function SeamlessTravelTo(NewPlayerState:APlayerState) : Void;
+  /**
+    session that the player needs to join/remove from as it is created/leaves
+  **/
+  public var SessionName : unreal.FName;
+  
 }
