ad29ba3e13d7983dddd235cc7d3b893f6491289f
ChetRippo <bdavis1000@gmail.com>
2016-08-28 13:44:57 -0400

    more FKey externs

--- Haxe/Externs/unreal/inputcore/FKey_Extra.hx
+++ Haxe/Externs/unreal/inputcore/FKey_Extra.hx
@@ -4,5 +4,7 @@ extern class FKey_Extra {
   @:uname(".ctor")
   public static function create() : FKey;
   public function GetDisplayName() : FText;
+  public function GetFName() : FName;
   public function IsGamepadKey() : Bool;
+  public function ToString() : FString;
 }
