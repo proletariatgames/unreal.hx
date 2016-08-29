2dea0fc1c2b3209fb28a63535cc8fabcddacd3f4
dogles <dan@proletariat.com>
2016-08-29 19:02:46 -0400

    collision externs

--- Haxe/Externs/unreal/FCollisionShape.hx
+++ Haxe/Externs/unreal/FCollisionShape.hx
@@ -2,5 +2,7 @@ package unreal;
 
 @:glueCppIncludes("WorldCollision.h")
 @:uextern extern class FCollisionShape {
+  static function MakeBox(BoxHalfExtent:Const<PRef<FVector>>):FCollisionShape;
+  static function MakeSphere(SphereRadius:Float32):FCollisionShape;
   static function MakeCapsule(CapsuleRadius:Float32, CapsuleHalfHeight:Float32):FCollisionShape;
 }
