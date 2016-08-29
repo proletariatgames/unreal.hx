93e4015685aed78b377638e06b522d4dae592961
ChetRippo <bdavis1000@gmail.com>
2016-08-28 20:03:14 -0400

    createByName FKey extern

--- Haxe/Externs/unreal/inputcore/FKey_Extra.hx
+++ Haxe/Externs/unreal/inputcore/FKey_Extra.hx
@@ -3,6 +3,8 @@ package unreal.inputcore;
 extern class FKey_Extra {
   @:uname(".ctor")
   public static function create() : FKey;
+  @:uname(".ctor")
+  public static function createByName(InName:FName) : FKey;
   public function GetDisplayName() : FText;
   public function GetFName() : FName;
   public function IsGamepadKey() : Bool;
