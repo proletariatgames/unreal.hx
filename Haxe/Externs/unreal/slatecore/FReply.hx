2715f25aa3ce0126097c74a58876760a45f17918
David J. Lennon <djlennon@gmail.com>
2016-09-01 10:30:55 -0400

    FReply.Handled() and Unhandled() externs

--- Haxe/Externs/unreal/slatecore/FReply.hx
+++ Haxe/Externs/unreal/slatecore/FReply.hx
@@ -4,5 +4,8 @@ package unreal.slatecore;
 @:umodule("SlateCore")
 @:glueCppIncludes("Input/Reply.h")
 @:noCopy @:noEquals @:uextern extern class FReply {
-  
+
+  static public function Handled() : FReply;
+  static public function Unhandled() : FReply;
+
 }
