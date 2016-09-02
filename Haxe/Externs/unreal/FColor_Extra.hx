ca2b9f69ec0789946a434b488c71ee9b7fafcb7e
David J. Lennon <djlennon@gmail.com>
2016-09-02 12:51:10 -0400

    FColor color constant externs (e.g. Red, Black, White)

--- Haxe/Externs/unreal/FColor_Extra.hx
+++ Haxe/Externs/unreal/FColor_Extra.hx
@@ -15,5 +15,19 @@ extern class FColor_Extra {
 
   @:thisConst
   public function ReinterpretAsLinear() : FLinearColor;
-}
 
+  public static var White(default,never) : FColor;
+  public static var Black(default,never) : FColor;
+  public static var Transparent(default,never) : FColor;
+  public static var Red(default,never) : FColor;
+  public static var Green(default,never) : FColor;
+  public static var Blue(default,never) : FColor;
+  public static var Yellow(default,never) : FColor;
+  public static var Cyan(default,never) : FColor;
+  public static var Magenta(default,never) : FColor;
+  public static var Orange(default,never) : FColor;
+  public static var Purple(default,never) : FColor;
+  public static var Turquoise(default,never) : FColor;
+  public static var Silver(default,never) : FColor;
+  public static var Emerald(default,never) : FColor;
+}
