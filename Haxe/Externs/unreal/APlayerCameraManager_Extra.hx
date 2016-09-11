03cf3d9b822ec3dddd42365eced2988089423af1
dogles <dan@proletariat.com>
2016-09-11 16:03:22 -0400

    APlayerCameraManager.SetViewTarget

--- Haxe/Externs/unreal/APlayerCameraManager_Extra.hx
+++ Haxe/Externs/unreal/APlayerCameraManager_Extra.hx
@@ -2,4 +2,11 @@ package unreal;
 
 extern class APlayerCameraManager_Extra {
   function UpdateCamera(DeltaTime:Float32) : Void;
+
+	/**
+	 * Sets a new ViewTarget.
+	 * @param NewViewTarget - New viewtarget actor.
+	 * @param TransitionParams - Optional parameters to define the interpolation from the old viewtarget to the new. Transition will be instant by default.
+	 */
+	function SetViewTarget(NewViewTarget:AActor, TransitionParams : FViewTargetTransitionParams) : Void;
 }
