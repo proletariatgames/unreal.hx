d020b5918b9315486feb0ac36011337a198c94de
paulproletariat <paul@proletariat.com>
2016-10-27 17:12:41 -0400

    Added internationalization extern

---
+++ Haxe/Externs/unreal/internationalization/Internationalization.hx
@@ -0,0 +1,11 @@
+package unreal.internationalization;
+
+@:global
+@:nocopy @:noEquals
+@:glueCppIncludes("Internationalization.h")
+@:uname("FInternationalization")
+@:uextern extern class FInternationalization {
+
+  public static function Get() : PRef<FInternationalization>;
+  public function SetCurrentCulture(Name:PRef<FString>) : Bool;
+}
