255a35088231945753d70be76b5dbce4c6c2e2c8
Cauê Waneck <waneck@gmail.com>
2016-11-02 19:55:05 -0200

    Add GetLog extern

--- Haxe/Externs/unreal/FGenericPlatformOutputDevices.hx
+++ Haxe/Externs/unreal/FGenericPlatformOutputDevices.hx
@@ -3,4 +3,5 @@ package unreal;
 @:glueCppIncludes("GenericPlatform/GenericPlatformOutputDevices.h")
 @:uextern extern class FGenericPlatformOutputDevices {
   static function GetAbsoluteLogFilename():FString;
+  static function GetLog():PPtr<FOutputDevice>;
 }
