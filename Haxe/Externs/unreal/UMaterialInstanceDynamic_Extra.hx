98154c3b0a40650f93c4db2b9992f41e93ec7594
ChetRippo <bdavis1000@gmail.com>
2016-11-16 14:02:55 -0500

    fix typo

--- Haxe/Externs/unreal/UMaterialInstanceDynamic_Extra.hx
+++ Haxe/Externs/unreal/UMaterialInstanceDynamic_Extra.hx
@@ -2,7 +2,7 @@ package unreal;
 
 extern class UMaterialInstanceDynamic_Extra
 {
-  public function SetScalarParameterValue(ParamterName:FName, Value:Float32) : Void;
+  public function SetScalarParameterValue(ParameterName:FName, Value:Float32) : Void;
   public function SetTextureParameterValue(ParameterName:FName, Value:UTexture) : Void;
   public function SetVectorParameterValue(ParameterName:FName, Value:FLinearColor) : Void;
   public function SetFontParameterValue(ParamaterName:FName, FontValue:UFont, FontPage:Int32) : Void;
