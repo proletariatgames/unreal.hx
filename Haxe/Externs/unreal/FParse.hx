9e504385e058f55a604e474f94f72cfa09074dae
djlennon <djlennon@gmail.com>
2016-11-28 15:44:03 -0500

    FParse.Param() extern

--- Haxe/Externs/unreal/FParse.hx
+++ Haxe/Externs/unreal/FParse.hx
@@ -3,4 +3,5 @@ package unreal;
 @:glueCppIncludes("Misc/Parse.h")
 @:uextern extern class FParse {
   static function Value(Stream:Const<TCharStar>, Match:Const<TCharStar>, Value:PRef<FString>, ?bShouldStopOnComma:Bool=true):Bool;
+  static function Param(Stream:Const<TCharStar>, Param:Const<TCharStar>):Bool;
 }
