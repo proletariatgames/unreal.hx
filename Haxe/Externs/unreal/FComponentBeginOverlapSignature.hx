a8d8936036615a4ae98b4bcb217a33400d8200d1
prolenorm <norm@proletariat.com>
2016-12-07 17:35:07 -0500

    Added extern for collision response delegate.

---
+++ Haxe/Externs/unreal/FComponentBeginOverlapSignature.hx
@@ -0,0 +1,3 @@
+package unreal;
+
+typedef FComponentBeginOverlapSignature = DynamicMulticastDelegate<FComponentBeginOverlapSignature, AActor->UPrimitiveComponent->Int32->Bool->Const<PRef<FHitResult>>->Void>;
--- Haxe/Externs/unreal/UPrimitiveComponent_Extra.hx
+++ Haxe/Externs/unreal/UPrimitiveComponent_Extra.hx
@@ -15,6 +15,16 @@ extern class UPrimitiveComponent_Extra {
   var OnComponentSleep:FComponentSleepSignature;
 
 	/**
+	 *	Event called when something starts to overlaps this component, for example a player walking into a trigger.
+	 *	For events when objects have a blocking collision, for example a player hitting a wall, see 'Hit' events.
+	 *
+	 *	@note Both this component and the other one must have bGenerateOverlapEvents set to true to generate overlap events.
+	 *	@note When receiving an overlap from another object's movement, the directions of 'Hit.Normal' and 'Hit.ImpactNormal'
+	 *	will be adjusted to indicate force from the other object against this object.
+	 */
+	var OnComponentBeginOverlap:FComponentBeginOverlapSignature;
+
+	/**
 	 *	Force all bodies in this component to sleep.
 	 */
 	function PutAllRigidBodiesToSleep():Void;
