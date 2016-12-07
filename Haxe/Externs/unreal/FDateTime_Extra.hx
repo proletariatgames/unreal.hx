5b53e529c201c711f7dfa61656ffd16e9e3cc9cc
djlennon <djlennon@gmail.com>
2016-12-07 18:43:12 -0500

    FDateTime externs

--- Haxe/Externs/unreal/FDateTime_Extra.hx
+++ Haxe/Externs/unreal/FDateTime_Extra.hx
@@ -5,7 +5,13 @@ extern class FDateTime_Extra {
   @:thisConst @:uname("ToString")
   public function toString() : FString;
 
+  @:thisConst @:uname("ToString")
+  public function toFormattedString(fmt:TCharStar) : FString;
+
   @:thisConst
   public function ToUnixTimestamp() : Int64;
 
+  public static function FromUnixTimestamp(unixTime:Int64) : FDateTime;
+
+  public function GetDate() : FDateTime;
 }
