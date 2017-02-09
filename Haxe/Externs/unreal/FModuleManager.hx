fe9d1659fec22c1de942b56045c2b70967677be7
Caue Waneck <waneck@gmail.com>
2017-02-09 22:11:42 +0000

    Don't use FModuleManager's private constructor

--- Haxe/Externs/unreal/FModuleManager.hx
+++ Haxe/Externs/unreal/FModuleManager.hx
@@ -1,7 +1,7 @@
 package unreal;
 
 @:glueCppIncludes("ModuleManager.h")
-@:uextern extern class FModuleManager {
+@:noCopy @:noEquals @:uextern extern class FModuleManager {
   static function GetModuleChecked<T>(name:Const<FName>):PRef<T>;
   static function LoadModulePtr<T>(name:Const<FName>):PPtr<T>;
 }
