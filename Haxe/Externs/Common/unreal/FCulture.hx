1dffacaf00ad1476409deaf0ce10c2eca3060d32
Caue Waneck <waneck@gmail.com>
2017-09-13 02:13:30 -0300

    Comment out FCulture::Create for now because of Linux
    Apparently ICU is now disabled on linux servers. However the way the
    externs are made still don't allow different externs per
    configuration/platform

--- Haxe/Externs/Common/unreal/FCulture.hx
+++ Haxe/Externs/Common/unreal/FCulture.hx
@@ -3,7 +3,7 @@ package unreal;
 @:glueCppIncludes("Internationalization/Culture.h")
 @:uname("FCulture")
 @:uextern extern class FCulture {
-  static function Create(localeName:Const<PRef<FString>>):TThreadSafeSharedPtr<FCulture>;
+  //static function Create(localeName:Const<PRef<FString>>):TThreadSafeSharedPtr<FCulture>;
   function GetRegion():Const<PRef<FString>>;
 }
 
