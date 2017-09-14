f388aaabf694f6b6b4c64922e218470c07c5c41d
Cauê Waneck <waneck@gmail.com>
2017-09-14 00:55:22 -0300

    Properly ignore deprecated declarations on clang

--- Haxe/Templates/Source/HaxeRuntime/Public/uhx/NoDeprecateHeader.h
+++ Haxe/Templates/Source/HaxeRuntime/Public/uhx/NoDeprecateHeader.h
@@ -1,5 +1,6 @@
 #ifdef __clang__
 #pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #elif _MSC_VER
 #pragma warning( disable : 4996 )
 #define _CRT_SECURE_NO_WARNINGS 1
