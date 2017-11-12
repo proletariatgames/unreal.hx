7ef74dbe9f5606eee6f36dd2799c0e62d6c49762
Prolebot <jenkins@proletariat.com>
2017-11-12 12:02:24 -0500

    [CL-14120] Update to latest perforce change Change 14120 by dan@dan_g3 on 2017/11/12 12:01:50
    [unreal.hx]
    FRotator functions
    FRadialDamageEvent ctor

--- Haxe/Externs/Common/unreal/FRadialDamageEvent_Extra.hx
+++ Haxe/Externs/Common/unreal/FRadialDamageEvent_Extra.hx
@@ -1,5 +1,8 @@
 package unreal;
 
 extern class FRadialDamageEvent_Extra {
+  public function new();
+  @:uname('.ctor') public static function create() : FRadialDamageEvent;
+
   public static var ClassID(get,never) : Int32;
 }
--- Haxe/Externs/Common/unreal/FRotator_Extra.hx
+++ Haxe/Externs/Common/unreal/FRotator_Extra.hx
@@ -14,9 +14,6 @@ extern class FRotator_Extra {
   @:uname('.ctor') public static function createFromQuat(quat:Const<PRef<FQuat>>):FRotator;
   @:uname('new') public static function createNewFromQuat(quat:Const<PRef<FQuat>>):POwnedPtr<FRotator>;
 
-  @:thisConst
-  public function GetInverse() : FRotator;
-
   public var Pitch:Float32;
   public var Yaw:Float32;
   public var Roll:Float32;
@@ -29,9 +26,185 @@ extern class FRotator_Extra {
   @:thisConst
   public function Vector() : FVector;
 
+	/**
+	 * Checks whether rotator is nearly zero within specified tolerance, when treated as an orientation.
+	 * This means that FRotator(0, 0, 360) is "zero", because it is the same final orientation as the zero rotator.
+	 *
+	 * @param Tolerance Error Tolerance.
+	 * @return true if rotator is nearly zero, within specified tolerance, otherwise false.
+	 */
+  @:thisConst
+  public function IsNearlyZero(Tolerance:Float32=1e-4) : Bool;
+
+	/**
+	 * Checks whether this has exactly zero rotation, when treated as an orientation.
+	 * This means that FRotator(0, 0, 360) is "zero", because it is the same final orientation as the zero rotator.
+	 *
+	 * @return true if this has exactly zero rotation, otherwise false.
+	 */
+  @:thisConst
+  public function IsZero() : Bool;
+
+	/**
+	 * Checks whether two rotators are equal within specified tolerance, when treated as an orientation.
+	 * This means that FRotator(0, 0, 360).Equals(FRotator(0,0,0)) is true, because they represent the same final orientation.
+	 *
+	 * @param R The other rotator.
+	 * @param Tolerance Error Tolerance.
+	 * @return true if two rotators are equal, within specified tolerance, otherwise false.
+	 */
+  @:thisConst
+  public function Equals(R:Const<PRef<FRotator>>, Tolerance:Float32=1e-4) : Bool;
+
+	/**
+	 * Adds to each component of the rotator.
+	 *
+	 * @param DeltaPitch Change in pitch. (+/-)
+	 * @param DeltaYaw Change in yaw. (+/-)
+	 * @param DeltaRoll Change in roll. (+/-)
+	 * @return Copy of rotator after addition.
+	 */
+  public function Add(DeltaPitch:Float32, DeltaYaw:Float32, DeltaRoll:Float32) : FRotator;
+
+	/**
+	 * Returns the inverse of the rotator.
+	 */
+  @:thisConst
+  public function GetInverse() : FRotator;
+
+	/**
+	 * Get the rotation, snapped to specified degree segments.
+	 *
+	 * @param RotGrid A Rotator specifying how to snap each component.
+	 * @return Snapped version of rotation.
+	 */
+  @:thisConst
+  public function GridSnap(RotGrid:Const<PRef<FRotator>>) : FRotator;
+
+	/**
+	 * Get Rotation as a quaternion.
+	 *
+	 * @return Rotation as a quaternion.
+	 */
+  @:thisConst
+  public function Quaternion() : FQuat;
+
+	/**
+	 * Convert a Rotator into floating-point Euler angles (in degrees). Rotator now stored in degrees.
+	 *
+	 * @return Rotation as a Euler angle vector.
+	 */
+  @:thisConst
+  public function Euler() : FVector;
+
+	/**
+	 * Rotate a vector rotated by this rotator.
+	 *
+	 * @param V The vector to rotate.
+	 * @return The rotated vector.
+	 */
+  @:thisConst
+  public function RotateVector(V:Const<PRef<FVector>>) : FVector;  
+
+	/**
+	 * Returns the vector rotated by the inverse of this rotator.
+	 *
+	 * @param V The vector to rotate.
+	 * @return The rotated vector.
+	 */
+  @:thisConst
+  public function UnrotateVector(V:Const<PRef<FVector>>) : FVector;
+
+	/**
+	 * Gets the rotation values so they fall within the range [0,360]
+	 *
+	 * @return Clamped version of rotator.
+	 */
+  @:thisConst
+  public function Clamp() : FRotator;  
+
+	/** 
+	 * Create a copy of this rotator and normalize, removes all winding and creates the "shortest route" rotation. 
+	 *
+	 * @return Normalized copy of this rotator
+	 */
+  @:thisConst
+  public function GetNormalized() : FRotator;
+
+	/** 
+	 * Create a copy of this rotator and denormalize, clamping each axis to 0 - 360. 
+	 *
+	 * @return Denormalized copy of this rotator
+	 */
+  @:thisConst
+  public function GetDenormalized() : FRotator;
+
+	/** Get a specific component of the vector, given a specific axis by enum */
+  @:thisConst
+  public function GetComponentForAxis(Axis:EAxis) : Float32;
+
+  /** Set a specified componet of the vector, given a specific axis by enum */
+  public function SetComponentForAxis(Axis:EAxis, Component:Float32) : Void;
+
+	/**
+	 * In-place normalize, removes all winding and creates the "shortest route" rotation.
+	 */
+  public function Normalize() : Void;
+
+	/** 
+	 * Decompose this Rotator into a Winding part (multiples of 360) and a Remainder part. 
+	 * Remainder will always be in [-180, 180] range.
+	 *
+	 * @param Winding[Out] the Winding part of this Rotator
+	 * @param Remainder[Out] the Remainder
+	 */
+  @:thisConst
+  public function GetWindingAndRemainder(Winding:PRef<FRotator>, Remainder:PRef<FRotator>) : Void;
+
+  	/**
+	 * Get a textual representation of the vector.
+	 *
+	 * @return Text describing the vector.
+	 */
+  @:thisConst
+  public function ToString() : FString;
+
+	/** Get a short textural representation of this vector, for compact readable logging. */
+  @:thisConst
+  public function ToCompactString() : FString;
+
+	/**
+	 * Initialize this Rotator based on an FString. The String is expected to contain P=, Y=, R=.
+	 * The FRotator will be bogus when InitFromString returns false.
+	 *
+	 * @param InSourceString	FString containing the rotator values.
+	 * @return true if the P,Y,R values were read successfully; false otherwise.
+	 */
+  public function InitFromString(InSourceString:Const<PRef<FString>>) : Bool;
+
+	/**
+	 * Utility to check if there are any non-finite values (NaN or Inf) in this Rotator.
+	 *
+	 * @return true if there are any non-finite values in this Rotator, otherwise false.
+	 */
+  @:thisConst
+  public function ContainsNaN() : Bool;
+
+	/**
+	 * Convert a vector of floating-point Euler angles (in degrees) into a Rotator. Rotator now stored in degrees
+	 *
+	 * @param Euler Euler angle vector.
+	 * @return A rotator from a Euler angle.
+	 */
+  public static function MakeFromEuler(Euler:Const<PRef<FVector>>) : FRotator;
+
   static var ZeroRotator(default, never):FRotator;
 
   @:op(A+B)
   @:expr(return createWithValues(Pitch + b.Pitch, Yaw + b.Yaw, Roll + b.Roll))
   public function _add(b:FRotator):FRotator;
+
+  @:op(A*B)
+  @:expr(return createWithValues(Pitch*b, Yaw*b, Roll*b))
+  public function _mul(b:Float32):FRotator;
 }
