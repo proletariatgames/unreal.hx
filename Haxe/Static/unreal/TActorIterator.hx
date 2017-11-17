152c6b0d24eb650b24f1f27cfe4b5374222386c1
Prolebot <jenkins@proletariat.com>
2017-11-17 16:37:26 -0500

    [CL-14186] Update to latest perforce change Change 14186 by dan@dan_g3 on 2017/11/17 16:34:02
    [unreal.hx] Add safer utiltiy functions to TActorIterator, so that the iterator gets cleaned up immediately (otherwise potentially causing a crash when GC occurs)

--- Haxe/Static/unreal/TActorIterator.hx
+++ Haxe/Static/unreal/TActorIterator.hx
@@ -22,6 +22,57 @@ typedef TActorIteratorImpl<T> = Dynamic;
   macro public static function createNew(?tParam:Expr, world:Expr) : Expr {
     return macro unreal.TActorIteratorImpl.createNew($tParam, $world);
   }
+
+  /** Safe version of iteration that promptly disposes the iterator.
+      @param tParam The type of actor to iterate.
+      @param world The world to look in.
+      @param fn The function to call for each item, with signature $tParam->Bool. Return false to break.
+   **/
+  macro public static function iterate<T>(tParam:ExprOf<unreal.TypeParam<T>>, world:Expr, fn:ExprOf<T->Bool>) : Expr {
+    return macro {
+      var it = unreal.TActorIteratorImpl.create($tParam, $world);
+      var fn = $fn;
+      try {
+        while (!it.op_Not()) {
+          if (!fn(it.op_Dereference())) {
+            break;
+          }
+          it.op_Increment();
+        }
+      } catch (e:Dynamic) {
+        it.dispose();
+        throw e;
+      }
+
+      it.dispose();
+    };
+  }
+
+  /** Safe version of iteration that promptly disposes the iterator.
+      @param tParam The type of actor to iterate.
+      @param world The world to look in.
+      @param class The subclass (UClass) to filter by.
+      @param fn The function to call for each item, with signature $tParam->Bool. Return false to break.
+   **/
+  macro public static function iterateForSubclass<T>(tParam:ExprOf<unreal.TypeParam<T>>, world:Expr, cls:Expr, fn:ExprOf<T->Bool>) : Expr {
+    return macro {
+      var it = unreal.TActorIteratorImpl.createForSubclass($tParam, $world, $cls);
+      var fn = $fn;
+      try {
+        while (!it.op_Not()) {
+          if (!fn(it.op_Dereference())) {
+            break;
+          }
+          it.op_Increment();
+        }
+      } catch (e:Dynamic) {
+        it.dispose();
+        throw e;
+      }
+
+      it.dispose();
+    };
+  }
 }
 
 private class TActorIteratorWrapper<T> {
