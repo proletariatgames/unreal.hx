ab7acce7729ba227cc42348f10b64beb70f31527
Caue Waneck <waneck@gmail.com>
2017-12-07 17:39:11 -0200

    Revert "[CL-14383] Update to latest perforce change"
    This reverts commit c5af3160a039f0ceeb3ca9f28dcb97ee3cc12df3.

--- Haxe/Templates/Source/HaxeRuntime/Private/uhx/DynamicClass.cpp
+++ Haxe/Templates/Source/HaxeRuntime/Private/uhx/DynamicClass.cpp
@@ -124,7 +124,6 @@ struct TClassCompiledInDefer<UHxBootstrap> : public FFieldCompiledInInfo
     UClassCompiledInDefer(this, InName, InClassSize, this->Crc);
   }
   virtual UClass* Register() const override {
-    check_hx_init();
     return UHxBootstrap::StaticClass();
   }
 
