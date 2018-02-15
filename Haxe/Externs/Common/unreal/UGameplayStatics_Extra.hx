9ad6421cacc633c915bd972ad3463ff487c25b16
Prolebot <jenkins@proletariat.com>
2018-02-15 15:17:33 -0500

    [CL-15663] Update to latest perforce change

--- Haxe/Externs/Common/unreal/UGameplayStatics_Extra.hx
+++ Haxe/Externs/Common/unreal/UGameplayStatics_Extra.hx
@@ -1,4 +1,6 @@
 package unreal;
 
 extern class UGameplayStatics_Extra {
+	@:ufunction(BlueprintCallable) @:uname("PlaySoundAtLocation")
+	static public function PlaySoundAtLocationWithOwner(WorldContextObject : unreal.Const<unreal.UObject>, Sound : unreal.USoundBase, Location : unreal.FVector, Rotation : unreal.FRotator, VolumeMultiplier : unreal.Float32 = 1.000000, PitchMultiplier : unreal.Float32 = 1.000000, StartTime : unreal.Float32 = 0.000000, AttenuationSettings : unreal.USoundAttenuation, ConcurrencySettings : unreal.USoundConcurrency, OwningActor : unreal.AActor) : Void;
 }
