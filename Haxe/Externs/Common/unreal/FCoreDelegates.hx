29c9fdd8025ecb25781a21d5a977be5bae9837ea
Prolebot <jenkins@proletariat.com>
2018-02-28 10:02:33 -0500

    [CL-16160] Update to latest perforce change Change 16160 by waneck@wnk-razer on 2018/02/28 10:00:58
    [unreal.hx] Delete old proletariat defines

--- Haxe/Externs/Common/unreal/FCoreDelegates.hx
+++ Haxe/Externs/Common/unreal/FCoreDelegates.hx
@@ -3,13 +3,4 @@ import unreal.*;
 
 @:glueCppIncludes('Misc/CallbackDevice.h')
 @:uextern extern class FCoreDelegates {
-#if proletariat
-  public static var ExternalArgumentsReceived:FExternalArgumentsReceived;
-#end
 }
-
-#if proletariat
-@:glueCppIncludes('Misc/CallbackDevice.h')
-@:uname('FCoreDelegates.FExternalArgumentsReceived')
-typedef FExternalArgumentsReceived = MulticastDelegate<FExternalArgumentsReceived, Const<PRef<FString>>->Void>;
-#end
