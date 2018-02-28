3746a9949e694910eaeeecf2e05e0a58f3076c34
Prolebot <jenkins@proletariat.com>
2018-02-27 22:46:22 -0500

    [CL-16157] Update to latest perforce change Change 16157 by waneck@wnk-razer on 2018/02/27 22:43:35
    [unreal.hx] Isolate proletariat-specific changes

--- Haxe/Externs/Common/unreal/UGameplayStatics_Extra.hx
+++ Haxe/Externs/Common/unreal/UGameplayStatics_Extra.hx
@@ -1,6 +1,8 @@
 package unreal;
 
 extern class UGameplayStatics_Extra {
+#if proletariat
 	@:ufunction(BlueprintCallable) @:uname("PlaySoundAtLocation")
 	static public function PlaySoundAtLocationWithOwner(WorldContextObject : unreal.Const<unreal.UObject>, Sound : unreal.USoundBase, Location : unreal.FVector, Rotation : unreal.FRotator, VolumeMultiplier : unreal.Float32 = 1.000000, PitchMultiplier : unreal.Float32 = 1.000000, StartTime : unreal.Float32 = 0.000000, AttenuationSettings : unreal.USoundAttenuation, ConcurrencySettings : unreal.USoundConcurrency, OwningActor : unreal.AActor) : Void;
+#end
 }
