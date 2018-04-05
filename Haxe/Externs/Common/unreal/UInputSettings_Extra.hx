54df3087b07ee7c4164a9b341c20bdf75e8e165c
Prolebot <jenkins@proletariat.com>
2018-04-05 09:47:29 -0400

    [CL-17821] Update to latest perforce change

--- Haxe/Externs/Common/unreal/UInputSettings_Extra.hx
+++ Haxe/Externs/Common/unreal/UInputSettings_Extra.hx
@@ -1,7 +1,4 @@
 package unreal;
 
 extern class UInputSettings_Extra {
-  function SaveKeyMappings() : Void;
-  function AddActionMapping(KeyMapping:Const<PRef<FInputActionKeyMapping>>) : Void;
-  function AddAxisMapping(KeyMapping:Const<PRef<FInputAxisKeyMapping>>) : Void;
 }
