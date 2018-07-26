ee1ea0971d9dc7ef6e8d2b51570787ab1935d15e
Prolebot <jenkins@proletariat.com>
2018-07-26 12:42:27 -0400

    [CL-22169] Update to latest perforce change

---
+++ Haxe/Externs/Common/unreal/FStepDownResult.hx
@@ -0,0 +1,8 @@
+package unreal;
+
+@:glueCppIncludes("GameFramework/CharacterMovementComponent.h")
+@:uname("UCharacterMovementComponent.FStepDownResult")
+@:uextern
+extern class FStepDownResult {
+	public function new();
+}
