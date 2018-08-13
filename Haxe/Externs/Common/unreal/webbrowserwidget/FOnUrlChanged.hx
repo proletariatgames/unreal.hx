86882aa11358997e66392c6e2b1b7f9ec7da7f4a
Prolebot <jenkins@proletariat.com>
2018-08-13 12:47:31 -0400

    [CL-22784] Update to latest perforce change

--- Haxe/Externs/Common/unreal/webbrowserwidget/FOnUrlChanged.hx
+++
@@ -1,5 +0,0 @@
-package unreal.webbrowserwidget;
-
-@:glueCppIncludes("UMG.h", "WebBrowser.h")
-@:uname('UWebBrowser.FOnUrlChanged')
-typedef FOnUrlChanged = DynamicMulticastDelegate<FOnUrlChanged,Const<PRef<FText>>->Void>;
