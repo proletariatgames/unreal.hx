b27dd5fa45d7d5d966920ca4a53be0202e7b54eb
Prolebot <jenkins@proletariat.com>
2018-09-07 11:47:47 -0400

    [CL-23974] Update to latest perforce change

--- Haxe/Externs/Common/unreal/DrawDebugHelpers.hx
+++ Haxe/Externs/Common/unreal/DrawDebugHelpers.hx
@@ -8,4 +8,5 @@ package unreal;
   /** Draw a debug box with rotation */
   @:global public static function DrawDebugBox(InWorld:UWorld, Center:Const<PRef<FVector>>, Extent:Const<PRef<FVector>>, Rotation:Const<PRef<FQuat>>, Color:Const<PRef<FColor>>, bPersistentLines:Bool /* = false */, LifeTime:Float32 /* = -1.f */, DepthPriority:UInt8 /* = 0 */, Thickness:Float32 /* = 0.f */):Void;
   @:global public static function DrawDebugCoordinateSystem(InWorld:UWorld, AxisLoc:Const<PRef<FVector>>, AxisRot:Const<PRef<FRotator>>, Scale:Float32, bPersistentLines:Bool /* = false */, LifeTime:Float32 /* = -1.f */,  DepthPriority:UInt8 /* = 0 */, Thickness:Float32 /* = 0.f */):Void;
+  @:global public static function DrawDebugDirectionalArrow(InWorld:UWorld, LineStart:Const<PRef<FVector>>, LineEnd:Const<PRef<FVector>>, ArrowSize:Float32, Color:Const<PRef<FColor>>, bPersistentLines:Bool /* = false*/, LifeTime:Float32 /* = -1.f*/, DepthPriority:UInt8 /* = 0*/, Thickness:Float32 /* = 0.f*/):Void;
 }
