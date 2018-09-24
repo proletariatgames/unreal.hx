571cf95bed07eb61c78491953b0294cf7e8df5f0
Prolebot <jenkins@proletariat.com>
2018-09-24 15:17:31 -0400

    [CL-24912] Update to latest perforce change

--- Haxe/Externs/Common/unreal/AVolume_Extra.hx
+++ Haxe/Externs/Common/unreal/AVolume_Extra.hx
@@ -3,5 +3,5 @@ package unreal;
 extern class AVolume_Extra {
 
   /** @returns true if a sphere/point (with optional radius CheckRadius) overlaps this volume */
-  public function EncompassesPoint(point:FVector, sphereRadius:Float32) : Bool;
+  public function EncompassesPoint(point:FVector, sphereRadius:Float32, OutDistanceToPoint:Ptr<Float32>) : Bool;
 }
