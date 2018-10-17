6fd2e95c96ed6be1a149e638e558b18e981a4c57
Prolebot <jenkins@proletariat.com>
2018-10-17 17:23:44 -0400

    [CL-26182] Update to latest perforce change

--- Haxe/Externs/Common/unreal/UAssetManager_Extra.hx
+++ Haxe/Externs/Common/unreal/UAssetManager_Extra.hx
@@ -2,6 +2,8 @@ package unreal;
 
 extern class UAssetManager_Extra {
   public static function Get() : PRef<UAssetManager>;
+	public function GetPrimaryAssetPath(AssetID:PRef<FPrimaryAssetId>) : FSoftObjectPath;
+	public function GetPrimaryAssetObject(AssetID:PRef<FPrimaryAssetId>) : UObject;
 	public function GetPrimaryAssetObjectList(PrimaryAssetType:FPrimaryAssetType , ObjectList:PRef<TArray<UObject>>) : Bool;
 	public function GetPrimaryAssetIdList(PrimaryAssetType:FPrimaryAssetType, PrimaryAssetIdList:PRef<TArray<FPrimaryAssetId>>) : Bool;
 }
