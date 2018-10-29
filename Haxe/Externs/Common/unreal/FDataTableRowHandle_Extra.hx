b84ff4f6aa443ed2b3dc21c257f30bcc0a3aa372
Prolebot <jenkins@proletariat.com>
2018-10-29 16:57:39 -0400

    [CL-26586] Update to latest perforce change

--- Haxe/Externs/Common/unreal/FDataTableRowHandle_Extra.hx
+++ Haxe/Externs/Common/unreal/FDataTableRowHandle_Extra.hx
@@ -4,4 +4,7 @@ extern class FDataTableRowHandle_Extra
 {
   @:thisConst
   public function GetRow<T>(ContextString : Const<PRef<FString>>) : PPtr<T>;
+
+  @:thisConst
+  public function IsNull() : Bool;
 }
