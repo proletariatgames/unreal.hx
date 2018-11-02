683e434cfff5ff23ef0145cc5b41a70b27e43afb
Prolebot <jenkins@proletariat.com>
2018-11-01 22:32:29 -0400

    [CL-26786] Update to latest perforce change

--- Haxe/Externs/Common/unreal/FVector2D_Extra.hx
+++ Haxe/Externs/Common/unreal/FVector2D_Extra.hx
@@ -1,6 +1,8 @@
 package unreal;
 
 extern class FVector2D_Extra {
+  public function new(x:Float32, y:Float32);
+
   @:uname('.ctor') public static function createWithValues(x:Float32, y:Float32):FVector2D;
   @:uname('new') public static function createNewWithValues(x:Float32, y:Float32):POwnedPtr<FVector2D>;
   @:uname('.ctor') public static function copyCreate(InVector2D:Const<PRef<FVector2D>>) : FVector2D;
