f1d0834ad90b65e0fe06f962eff533dad05528ef
Prolebot <jenkins@proletariat.com>
2018-12-17 13:52:35 -0500

    [CL-28504] Update to latest perforce change

--- Haxe/Externs/Common/unreal/UAssetManager_Extra.hx
+++ Haxe/Externs/Common/unreal/UAssetManager_Extra.hx
@@ -6,4 +6,5 @@ extern class UAssetManager_Extra {
 	public function GetPrimaryAssetObject(AssetID:PRef<FPrimaryAssetId>) : UObject;
 	public function GetPrimaryAssetObjectList(PrimaryAssetType:FPrimaryAssetType , ObjectList:PRef<TArray<UObject>>) : Bool;
 	public function GetPrimaryAssetIdList(PrimaryAssetType:FPrimaryAssetType, PrimaryAssetIdList:PRef<TArray<FPrimaryAssetId>>) : Bool;
+	@:thisConst public function GetPrimaryAssetIdForObject(Object:PPtr<UObject>) : FPrimaryAssetId;
 }
