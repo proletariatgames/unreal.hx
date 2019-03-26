912f7d50f2eea55fc4b8861c05722100daf02431
Prolebot <jenkins@proletariat.com>
2019-03-26 14:07:29 -0400

    [CL-32013] Update to latest perforce change

--- Haxe/Externs/Common/unreal/UAssetManager_Extra.hx
+++ Haxe/Externs/Common/unreal/UAssetManager_Extra.hx
@@ -6,5 +6,6 @@ extern class UAssetManager_Extra {
 	public function GetPrimaryAssetObject(AssetID:PRef<FPrimaryAssetId>) : UObject;
 	public function GetPrimaryAssetObjectList(PrimaryAssetType:FPrimaryAssetType , ObjectList:PRef<TArray<UObject>>) : Bool;
 	public function GetPrimaryAssetIdList(PrimaryAssetType:FPrimaryAssetType, PrimaryAssetIdList:PRef<TArray<FPrimaryAssetId>>) : Bool;
-	@:thisConst public function GetPrimaryAssetIdForObject(Object:PPtr<UObject>) : FPrimaryAssetId;
+	public function GetPrimaryAssetDataList(PrimaryAssetType:FPrimaryAssetType, PrimaryAssetList:PRef<TArray<unreal.assetregistry.FAssetData>>) : Bool;
+	@:thisConst public function GetPrimaryAssetIdForObject(Object:UObject) : FPrimaryAssetId;
 }
--- Haxe/Externs/Common/unreal/assetregistry/FAssetData_Extra.hx
+++ Haxe/Externs/Common/unreal/assetregistry/FAssetData_Extra.hx
@@ -1,5 +1,10 @@
 package unreal.assetregistry;
 
 extern class FAssetData_Extra {
+  @:thisConst
   public function GetAsset():UObject;
+  @:thisConst
+  public function GetFullName() : FString;
+  @:thisConst
+  public function GetPrimaryAssetId() : FPrimaryAssetId;
 }
