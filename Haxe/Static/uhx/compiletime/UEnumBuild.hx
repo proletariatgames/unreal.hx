432a21499fed8aa5dfca6070f81c078d5b754094
Prolebot <jenkins@proletariat.com>
2019-07-01 09:08:46 -0400

    [CL-36545] Update to latest perforce change Change 36545 by waneck@wnk-razer on 2019/07/01 09:04:31
    [unreal.hx] Temporarily disable fast enum lookup
    
    Closes SB-5334

--- Haxe/Static/uhx/compiletime/UEnumBuild.hx
+++ Haxe/Static/uhx/compiletime/UEnumBuild.hx
@@ -149,30 +149,31 @@ class UEnumBuild
   }
 
   public static function createEnumIndex(enumType:Expr, index:Expr):Expr {
-    var t = Context.follow(Context.typeof(enumType));
-    switch(t) {
-    case TAnonymous(_.get() => { status: AEnumStatics(e) }):
-      var helperName = e.toString() + '_FastIndex';
-      try {
-        Context.getType(helperName);
-      } catch(exc:Dynamic) {
-        // create the type
-        var en = e.get(),
-            fullName = en.module + '.' + en.name;
-        var values = [ for (name in en.names) Context.parse(fullName + '.' + name, en.pos) ];
-        var expr = { expr:EArrayDecl(values), pos:enumType.pos };
-        var cls = macro class {
-          public static var all = $expr;
-        }
-        cls.pack = en.pack;
-        cls.name = en.name + '_FastIndex';
-        Globals.cur.hasUnprocessedTypes = true;
-        Context.defineType(cls);
-      }
-      var expr = Context.parse(helperName + '.all', enumType.pos);
-      return macro $expr[$index];
-    case _:
+    // TODO re-enable this when figure out the missing symbol issue
+    // var t = Context.follow(Context.typeof(enumType));
+    // switch(t) {
+    // case TAnonymous(_.get() => { status: AEnumStatics(e) }):
+    //   var helperName = e.toString() + '_FastIndex';
+    //   try {
+    //     Context.getType(helperName);
+    //   } catch(exc:Dynamic) {
+    //     // create the type
+    //     var en = e.get(),
+    //         fullName = en.module + '.' + en.name;
+    //     var values = [ for (name in en.names) Context.parse(fullName + '.' + name, en.pos) ];
+    //     var expr = { expr:EArrayDecl(values), pos:enumType.pos };
+    //     var cls = macro class {
+    //       public static var all = $expr;
+    //     }
+    //     cls.pack = en.pack;
+    //     cls.name = en.name + '_FastIndex';
+    //     Globals.cur.hasUnprocessedTypes = true;
+    //     Context.defineType(cls);
+    //   }
+    //   var expr = Context.parse(helperName + '.all', enumType.pos);
+    //   return macro $expr[$index];
+    // case _:
       return macro Type.createEnumIndex($enumType, $index);
-    }
+    // }
   }
 }
