76dbf6e3513f188d599bb4ef011722fe2e9d396c
Prolebot <jenkins@proletariat.com>
2020-02-11 15:47:47 -0500

    [CL-51144] Update to latest perforce change

--- Haxe/Externs/Common/unreal/umg/UScrollBox_Extra.hx
+++ Haxe/Externs/Common/unreal/umg/UScrollBox_Extra.hx
@@ -4,4 +4,6 @@ import unreal.slatecore.*;
 
 extern class UScrollBox_Extra {
 	public function GetViewFraction() : Float32;
+
+	public function GetWidgetScrollOffset(WidgetToFind : UWidget, @:opt("IntoView") ScrollDestination : unreal.slate.EDescendantScrollDestination) : Float32;
 }
