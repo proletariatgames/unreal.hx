8f05ab27ea38ee8770226824e069b6325b286edd
Prolebot <jenkins@proletariat.com>
2020-06-25 19:36:36 -0400

    [CL-62799] Update to latest perforce change

--- Haxe/Externs/UE4.22/unreal/animationbudgetallocator/USkeletalMeshComponentBudgeted_Extra.hx
+++ Haxe/Externs/UE4.22/unreal/animationbudgetallocator/USkeletalMeshComponentBudgeted_Extra.hx
@@ -2,6 +2,11 @@ package unreal.animationbudgetallocator;
 
 extern class USkeletalMeshComponentBudgeted_Extra
 {
+	@:thisConst
+	public function GetShouldUseActorRenderedFlag() : Bool;
+
+	public function SetShouldUseActorRenderedFlag(value:Bool) : Void;
+
 	/** Updates significance budget if this component has been registered with a AnimationBudgetAllocator */
 	public function SetComponentSignificance(Significance:Float32, bNeverSkip:Bool = false, bTickEvenIfNotRendered:Bool = false, bAllowReducedWork:Bool = true, bForceInterpolate:Bool = false) : Void;
 }
